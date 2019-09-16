f4b2817ec5c86cc7821ae151d6f233963416740d
Arushi Rai <arushirai1999@gmail.com>
2019-09-16 14:45:04 -0500

    Runs folium

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
+import folium
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    start_coords = (41.8333722, -87.628204)
+    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    return folium_map._repr_html_()
